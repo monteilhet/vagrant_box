1944514273529b0af3930bb29c600d83f908691a
arnaud.monteilhet <amonteilhet@groupeastek.fr>
2022-03-06 15:57:47 +0100

    update vbox_guest install

--- vb_guest/vbox_guest_install.sh
+++ vb_guest/vbox_guest_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # by default use --nox11
-X11='--nox11'
-[[ $1 != "" ]] && X11=
+args=${1:---nox11}
 
 apt-get purge virtualbox-guest-dkms virtualbox-guest-source virtualbox-guest-utils virtualbox-guest-X11
 apt-get install -y dkms build-essential module-assistant
@@ -11,4 +10,4 @@ m-a prepare
 mkdir -p /media/cdrom
 mount /dev/sr0 /media/cdrom
 cd /media/cdrom
-sh VBoxLinuxAdditions.run $X11
+sh VBoxLinuxAdditions.run $args
--- vb_guest/vbox_guest_install_rhel.sh
+++ vb_guest/vbox_guest_install_rhel.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # by default use --nox11
-X11='--nox11'
-[[ $1 != "" ]] && X11=
+args=${1:---nox11}
 
 # yum -y update kernel
 yum install -y epel-release
 export KERN_DIR="/usr/src/kernels/$(ls /usr/src/kernels/)"
 yum -y install kernel-devel kernel-headers dkms bzip2 gcc gcc-c++
 mount /dev/sr0 /media ; cd /media
-sh ./VBoxLinuxAdditions.run $X11
+sh ./VBoxLinuxAdditions.run $args
